d5f50abdd706575c9f685861f728d334bf10c2cb
gregory <gregory.qian@youcanrobot.com>
2018-08-22 14:52:43 +0800

    Initial Commit

---
+++ ServiceDemoForStackOverflow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ServiceDemoForStackOverflow
+//
+//  Created by Gregory Qian on 2018/8/22.
+//  Copyright © 2018 Gregory Qian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ServiceDemoForStackOverflow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ServiceDemoForStackOverflow
+//
+//  Created by Gregory Qian on 2018/8/22.
+//  Copyright © 2018 Gregory Qian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
